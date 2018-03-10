56e15545d965d416bb41983fc15767fc7d0d1e96
UristLikot <33843680+UristLikot@users.noreply.github.com>
2018-03-11 02:36:19 +0300

    Deploy v2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 updater = Updater(token='459744558:AAEWYIqK8c-VTomDzIm_4Vre0f-bCJL0JXs')
 dispatcher = updater.dispatcher
@@ -21,21 +22,42 @@ def roll(s):
 
 
 def startCommand(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text='Задайте значение')
+    bot.send_message(chat_id=update.message.chat_id, text="hey")
+    bot.send_message(chat_id=update.message.chat_id, text="hey")
+    keyboar(bot, update)
     return update.message.text
 
 
+def keyboar(bot, update):
+    keyboard = [[InlineKeyboardButton("Option 1", callback_data='1'),
+                 InlineKeyboardButton("Option 2", callback_data='2')],
+
+                [InlineKeyboardButton("Option 3", callback_data='3')]]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text('Please choose:', reply_markup=reply_markup)
+    bot.send_message(chat_id=update.message.chat_id, reply_markup=keyboard)
+
+
+def button(bot, update):
+    query = update.callback_query
+
+    bot.send_message(text="Choice : {}".format(query.data),
+                     chat_id=query.message.chat_id,
+                     message_id=query.message.message_id)
 def textMessage(bot, update):
     dd=roll(update.message.text)
     response = 'Результат ' + dd
     bot.send_message(chat_id=update.message.chat_id, text=response)
 
-
 start_command_handler = CommandHandler('start', startCommand)
 text_message_handler = MessageHandler(Filters.text, textMessage)
+callback_handler = CallbackQueryHandler(button)
 
 dispatcher.add_handler(start_command_handler)
 dispatcher.add_handler(text_message_handler)
+dispatcher.add_handler(callback_handler)
 
 updater.start_polling(clean=True)
 
